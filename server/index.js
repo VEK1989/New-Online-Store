090ac797141741cd75f6d12a416351b5e8da0ad2
VEK1989 <55896799+VEK1989@users.noreply.github.com>
2021-10-07 17:14:55 +0300

    Добавил страницу магазина в cors

--- server/index.js
+++ server/index.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
 const express = require('express')
+const cors = require('cors')
 const sequelize = require('./db')
 const models = require('./models/models')
-const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const fileUpload = require('express-fileupload')
 const router = require('./routes/index')
@@ -12,7 +12,10 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 const app = express()
 
-app.use(cors())
+app.use(cors({
+	credentials: true,
+	origin: process.env.CLIENT_URL
+}))
 app.use(express.json())
 app.use(cookieParser())
 app.use(express.static(path.resolve(__dirname, 'static')))
