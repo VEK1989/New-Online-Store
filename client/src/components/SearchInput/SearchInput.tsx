4a148238625db1c5e6120620d0310588c4ab1d54
VEK1989 <55896799+VEK1989@users.noreply.github.com>
2021-10-20 09:56:57 +0300

    Доделал поиск по автору

--- client/src/components/SearchInput/SearchInput.tsx
+++ client/src/components/SearchInput/SearchInput.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { useTypedSelector } from '../../hooks/useTypedSelector'
-import { GenreAuthorActionCreator } from '../../store/action-creators/genreAuthorActionCreator'
 import { GoodsActionCreator } from '../../store/action-creators/goodsActionCreator'
 import MyInput from '../ui/MyInput/MyInput'
 import style from './SearchInput.module.css'
@@ -9,32 +8,42 @@ import style from './SearchInput.module.css'
 const SearchInput: React.FC = () => {
 	const [search, setSearch] = useState('')
 	const authors = useTypedSelector(state => state.genreAuthor.author)
-	const selectAuthor = useTypedSelector(state => state.genreAuthor.selectedAuthor)
+	const { limit, page } = useTypedSelector(state => state.goods)
 	const dispatch = useDispatch()
 
 	const filterAuthorByName = authors.filter(author => {
 		return author.name.toLowerCase().includes(search.toLowerCase())
 	})
 
-	useEffect(() => {
-		let filterParam = filterAuthorByName[0]  //плохой код надо передумать
-		if (!search) {
-			filterParam = filterAuthorByName[10]
-		}
-		console.log(filterParam)
-		dispatch(GenreAuthorActionCreator.setSelectedAuthor(filterParam))
-		dispatch(GoodsActionCreator.setPage(1))
-	}, [filterAuthorByName[0]])
+	const selectAuthor = (id: number) => {
+		dispatch(GoodsActionCreator.getAllGoods(id, null, limit, page))
+		setSearch('')
+	}
 
 	return (
-		<div className={style.searchBlock}>
+		<form className={style.searchBlock}>
 			<MyInput
 				value={search}
 				type='text'
 				placeholder='Поиск по имени автора'
 				onChange={e => setSearch(e.target.value)}
 			/>
-		</div>
+			<ul className={style.autocomplete}>
+				{
+					search
+						? filterAuthorByName.map(item => {
+							return <li
+								key={item.id}
+								className={style.autocomleteItem}
+								onClick={() => selectAuthor(item.id)}
+							>
+								{item.name}
+							</li>
+						})
+						: null
+				}
+			</ul>
+		</form>
 	);
 };
 
