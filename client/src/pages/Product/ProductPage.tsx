068f067168dccd44efdfd0ed26967172e9b2c784
VEK1989 <55896799+VEK1989@users.noreply.github.com>
2021-10-22 10:28:43 +0300

    Кнопка В корзину на странице товара теперь работает

--- client/src/pages/Product/ProductPage.tsx
+++ client/src/pages/Product/ProductPage.tsx
@@ -1,21 +1,37 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
 import { useLocation } from 'react-router'
 import StarRating from '../../components/StarRating/StarRating'
 import MyButton from '../../components/ui/MyButton/MyButton'
 import { useTypedSelector } from '../../hooks/useTypedSelector'
+import { CartActionCreator } from '../../store/action-creators/cartActionCreator'
 import { GoodsActionCreator } from '../../store/action-creators/goodsActionCreator'
 import style from './ProductPage.module.css'
 
 const ProductPage: React.FC = () => {
 	const location = useLocation()
 	const dispatch = useDispatch()
+	const [isInCart, setIsInCart] = useState(false)
 	const product = useTypedSelector(state => state.goods.product)
+	let { countGoods } = useTypedSelector(state => state.cart)
+	const cart = useTypedSelector(state => state.cart.cart)
+	const id = Number(location.pathname.split('/')[2])
 
 	useEffect(() => {
-		const id = Number(location.pathname.split('/')[2])
 		dispatch(GoodsActionCreator.getOneProduct(id))
+		const item = cart.find(item => {
+			return item.id == id
+		})
+		if (item) {
+			setIsInCart(true)
+		}
 	}, [])
+
+	const inCart = (id: number) => {
+		dispatch(CartActionCreator.putProductInMyCart(id))
+		dispatch(CartActionCreator.setCountGoods(countGoods += 1))
+		setIsInCart(true)
+	}
 
 	return (
 		<div className={style.product}>
@@ -31,7 +47,11 @@ const ProductPage: React.FC = () => {
 						</div>
 					</div>
 					<div>{product.info}</div>
-					<MyButton>Добавить в корзину</MyButton>
+					{
+						isInCart
+							? <MyButton disabled > &#10004; В корзине</MyButton>
+							: <MyButton onClick={() => inCart(id)}>Добавить в корзину</MyButton>
+					}
 				</div>
 			</div>
 			<h2>Аннотация книги "{product.name}"</h2>
